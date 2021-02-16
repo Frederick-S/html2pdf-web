662fdd300c1ac8d8074bdcf625970509e7a0e563
Frederick-S <mao_xiaodan@hotmail.com>
2021-02-16 20:33:29 +0800

    ui

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,30 @@
 <template>
-  <div class="container">
+  <div class="container is-one-third column">
+    <section>
+      <b-field label="Format">
+        <b-select v-model="format">
+          <option v-for="format in formats" :value="format" :key="format">{{ format }}</option>
+        </b-select>
+      </b-field>
+      <b-field label="Html">
+        <b-input type="textarea" v-model="html"></b-input>
+      </b-field>
+      <b-field class="is-grouped is-grouped-centered">
+        <b-button type="is-primary">Generate</b-button>
+      </b-field>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  props: {
-    html: null
+  data () {
+    return {
+      html: null,
+      format: 'Letter',
+      formats: ['Letter', 'Legal', 'Tabloid', 'Ledger', 'A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6']
+    }
   }
 }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 
@@ -9,6 +8,5 @@ Vue.config.productionTip = false
 Vue.use(Buefy)
 
 new Vue({
-  store,
   render: h => h(App)
 }).$mount('#app')
