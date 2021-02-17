64509a1965b0b3ce0e6fb08b7dbfcfc5709d7f0b
Frederick-S <mao_xiaodan@hotmail.com>
2021-02-17 09:53:40 +0800

    generate pdf

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,13 +10,16 @@
         <b-input type="textarea" v-model="html"></b-input>
       </b-field>
       <b-field class="is-grouped is-grouped-centered">
-        <b-button type="is-primary">Generate</b-button>
+        <b-button type="is-primary" @click="generate">Generate</b-button>
       </b-field>
     </section>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import fileDownload from 'js-file-download'
+
 export default {
   name: 'Home',
   data () {
@@ -24,6 +27,20 @@ export default {
       html: null,
       format: 'Letter',
       formats: ['Letter', 'Legal', 'Tabloid', 'Ledger', 'A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6']
+    }
+  },
+  methods: {
+    generate () {
+      axios.post('https://html2pdf101.azurewebsites.net/pdfs', {
+        html: this.html,
+        format: this.format
+      }, {
+        responseType: 'blob'
+      }).then(response => {
+        fileDownload(response.data, 'generated.pdf')
+      }).catch(error => {
+        console.error(error)
+      })
     }
   }
 }
